2c4135de783288ad5878cd787fac343a54ececc1
rohith2016 <rohithk2016@gmail.com>
2019-03-24 23:33:09 +0530

    frontpage, proptypes, productbox done

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,32 +1,34 @@
-import React, { Component } from 'react'
-import Product from './Product'
-import Title from './Title'
-import {storeProducts} from '../data'
-import {Consumer} from '../context'
 
+import React, { Component } from "react";
+import Product from "./Product";
+import Title from "./Title";
+import { storeProducts } from "../data";
+import styled from "styled-components";
+import { ProductConsumer } from "../context";
 export default class ProductList extends Component {
-
-    state={
-        products:storeProducts
-    }
-
-    render(){
-        console.log(this.state.products)
-        return(
-            <React.Fragment>
-                <div className="py-5">
-                    <div className="container">
-                        <Title name="our" title="products"/>
-                        <div className="row">
-                        <Consumer>
-                            {(value)=>{
-                                return <h1>{value}</h1>;
-                            }}
-                        </Consumer>
-                        </div>
-                    </div>
-                </div>
-            </React.Fragment>
-        );
-    }
+  state = {
+    products: storeProducts
+  };
+  render() {
+    return (
+      <React.Fragment>
+        <ProductWrapper className="py-5">
+          <div className="container">
+            <Title name="our" title="products" />
+            <div className="row">
+              <ProductConsumer>
+                {value => {
+                  return value.products.map(product => {
+                    return <Product key={product.id} product={product} />;
+                  });
+                }}
+              </ProductConsumer>
+            </div>
+          </div>
+        </ProductWrapper>
+      </React.Fragment>
+    );
+  }
 }
+
+const ProductWrapper = styled.section``;
--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
-import { Provider } from "./context";
+import { ProductProvider } from "./context";
 
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-    <Provider>
+    <ProductProvider>
         <Router>
             <App />
         </Router>
-    </Provider>
+    </ProductProvider>
     , document.getElementById('root'));
 
 // ReactDOM.render(
