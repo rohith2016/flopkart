673fbc251a43a900694c9954ea61bb5ff2b4ad4d
rohith2016 <rohithk2016@gmail.com>
2019-03-24 17:11:07 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import {Switch, Route} from 'react-router-dom';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Navbar from './components/Navbar'
+import Cart from './components/Cart'
+import Details from './components/Details'
+import ProductList from './components/ProductList'
+import Default from './components/Default'
+
+
+
+class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <Navbar></Navbar>
+        <Switch>
+          <Route exact path="/" component={ProductList}/>
+          <Route path="/details" component={Details}/>
+          <Route path="/cart" component={Cart}/>
+          <Route component={Default}/>
+        </Switch>
+      </React.Fragment>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react'
+import {Link} from 'react-router-dom'
+
+import logo from '../logo.svg';
+
+
+export default class Navbar extends Component {
+    render(){
+        return(
+            <nav className="navbar navbar-expand-sm bg-info navbar-dark px-sm-5">
+                <Link>
+                    <img src={logo} alt="logo" className="navbar-brand"></img>
+                </Link>
+                <ul className="navbar-nav align-items-center">
+                    <li className="nav-item ml-5">
+                        <Link to="/" className="nav-link">
+                            Products
+                        </Link>
+                    </li>
+                </ul>
+                <Link to='/cart' className="ml-auto">
+                    <button>
+                        <i className="fas fa-cart-plus"/>
+                        My Cart
+                    </button>
+                </Link>
+            </nav>
+        );
+    }
+}
